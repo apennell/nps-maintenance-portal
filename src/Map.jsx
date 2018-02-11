b97e6ef37c03713f4891cd299c6b50d4a5c89eaf
Annie Pennell <anniecpennell@gmail.com>
2018-02-10 19:20:40 -0800

    created react app and implemented basic mapbox map

---
+++ src/Map.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import mapboxgl from 'mapbox-gl';
+ 
+
+class Map extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lng: 5,
+      lat: 34,
+      zoom: 1.5
+    };
+  }
+
+  componentDidMount() {
+    const { lng, lat, zoom } = this.state;
+
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v10',
+      center: [lng, lat],
+      zoom
+    });
+
+    map.on('move', () => {
+      const { lng, lat } = map.getCenter();
+
+      this.setState({
+        lng: lng.toFixed(4),
+        lat: lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2)
+      });
+    });
+  }
+
+  render() {
+    const { lng, lat, zoom } = this.state;
+
+    return (
+      <div>
+        <div ref={el => this.mapContainer = el} className="absolute top right left bottom" />
+      </div>
+    );
+  }
+}
+
+export default Map;
